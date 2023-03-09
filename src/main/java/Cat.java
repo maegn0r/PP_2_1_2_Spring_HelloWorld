19dad11542fdc64a66e0f193c38dbb086425902c
novitskynick <novitskynick@gmail.com>
2023-03-09 15:22:49 +0300

    homework done

---
+++ src/main/java/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+    private String name;
+
+    Cat() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
